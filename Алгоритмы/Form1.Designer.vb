379ebe2f7a857794664835054eceeda7953c05db
Denis Shurmelev <63847580+ErichHartmann0@users.noreply.github.com>
2020-05-13 15:36:24 +0300

    Add files via upload

---
+++ Алгоритмы/Form1.Designer.vb
@@ -0,0 +1,312 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.CommandStart = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Axis1 = New myUCLib.Axis()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'CommandStart
+        '
+        Me.CommandStart.Location = New System.Drawing.Point(12, 12)
+        Me.CommandStart.Name = "CommandStart"
+        Me.CommandStart.Size = New System.Drawing.Size(115, 37)
+        Me.CommandStart.TabIndex = 0
+        Me.CommandStart.Text = "CommandStart"
+        Me.CommandStart.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 68)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 107)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 145)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(12, 185)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox4.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(138, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Введите значение Alpha0:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Введите значение V0:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(116, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Введите значение Dt:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 169)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(111, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Введите значение h:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(12, 225)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox5.TabIndex = 9
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(12, 264)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox6.TabIndex = 10
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(12, 303)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox7.TabIndex = 11
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(12, 342)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox8.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 209)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(131, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Введите значение Xmax:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 248)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(131, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Введите значение Ymax:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 287)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(131, 13)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Введите значение Vmax:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 326)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(108, 13)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Введите значение t:"
+        '
+        'Axis1
+        '
+        Me.Axis1.axis_bkcolor = System.Drawing.Color.White
+        Me.Axis1.axis_color = System.Drawing.Color.Gray
+        Me.Axis1.Axis_Type = CType(1, Byte)
+        Me.Axis1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Axis1.E_x = 0
+        Me.Axis1.E_y = 0
+        Me.Axis1.Location = New System.Drawing.Point(284, 13)
+        Me.Axis1.Name = "Axis1"
+        Me.Axis1.Pix_color = System.Drawing.Color.Black
+        Me.Axis1.Pix_Size = 2.0!
+        Me.Axis1.Pix_type = CType(1, Byte)
+        Me.Axis1.Size = New System.Drawing.Size(466, 379)
+        Me.Axis1.TabIndex = 17
+        Me.Axis1.x_Base = 1.0!
+        Me.Axis1.x_Name = "X"
+        Me.Axis1.y_Base = 1.0!
+        Me.Axis1.y_Name = "Y"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(171, 68)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(81, 20)
+        Me.TextBox9.TabIndex = 18
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(171, 107)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(81, 20)
+        Me.TextBox10.TabIndex = 19
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(163, 52)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(115, 13)
+        Me.Label9.TabIndex = 20
+        Me.Label9.Text = "Введите значение  X:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(163, 91)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(115, 13)
+        Me.Label10.TabIndex = 21
+        Me.Label10.Text = "Введите значение  Y:"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 368)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(15, 14)
+        Me.CheckBox1.TabIndex = 22
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(799, 486)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.Axis1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CommandStart)
+        Me.Name = "Form1"
+        Me.Text = "Под углом к горизонту"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CommandStart As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Axis1 As myUCLib.Axis
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ Алгоритмы/Form1.vb
@@ -0,0 +1,95 @@
+﻿Option Explicit On
+Imports System.Drawing.Color
+Imports System.Math
+Public Class Form1
+    Const Pi = 3.14
+    Dim x As Single
+    Dim y As Single
+    Dim x0 As Single
+    Dim y0 As Single
+    Dim v As Single
+    Dim v0 As Single
+    Dim vx0 As Single
+    Dim vy0 As Single
+    Dim vx As Single
+    Dim vy As Single
+    Dim alpha As Single
+    Dim alpha0 As Single
+    Dim ax As Single
+    Dim ay As Single
+    Dim t As Single
+    Dim dt As Single
+    Dim PixColor As Color
+    Dim Xmax As Single
+    Dim Ymax As Single
+    Dim Vmax As Single
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles CommandStart.Click
+        alpha0 = Val(TextBox1.Text)
+        v0 = Val(TextBox2.Text)
+        dt = Val(TextBox3.Text)
+        t = 0
+        v = v0
+        vx0 = v0 * Cos(alpha0 * Pi / 180)
+        vy0 = v0 * Sin(alpha0 * Pi / 180)
+        x0 = 0
+        y0 = Val(TextBox4.Text)
+        Vmax = v0
+        Xmax = x0
+        Ymax = y0
+        Timer1.Enabled = True
+    End Sub
+    Private Sub InitAxis()
+        Axis1.Axis_Type = 3
+        Axis1.Pix_type = 1
+        Axis1.x_Base = Val(TextBox9.Text)
+        Axis1.y_Base = Val(TextBox10.Text)
+        Axis1.Pix_type = 3
+        Axis1.Pix_Size = 0.05
+        Axis1.x_Name = "X"
+        Axis1.y_Name = "Y"
+        Axis1.BackColor = White
+        Axis1.AxisDraw()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        t = t + dt
+        If CheckBox1.Checked Then
+            ax = -v * vx
+            ay = -1 - v * vy
+            PixColor = Black
+        Else
+            ax = 0
+            ay = -1
+            PixColor = Red
+        End If
+        vx = vx0 + ax * dt
+        vy = vy0 + ay * dt
+        x = x0 + (vx0 + vx) * dt + ax * dt * dt / 2
+        y = y0 + (vy0 + vy) * dt + ay * dt * dt / 2
+        'траектория
+        Axis1.Pix_Size = 0.005
+        Axis1.PixDraw(x, y, PixColor, 1)
+        Axis1.StatToDin()
+        'ядро
+        Axis1.Pix_Size = 0.02
+        Axis1.PixDraw(x, y, PixColor, 2)
+        Axis1.DinToPic()
+        If y < 0 Then
+            Timer1.Enabled = False
+            TextBox5.Text = Str(Round(Xmax, 2))
+            TextBox6.Text = Str(Round(Ymax, 2))
+            TextBox7.Text = Str(Round(Vmax, 2))
+        End If
+        x0 = x
+        y0 = y
+        vx0 = vx
+        vy0 = vy
+        v = Sqrt(vx ^ 2 + vy ^ 2)
+        If Abs(x) > Xmax Then Xmax = Abs(x)
+        If Abs(y) > Ymax Then Ymax = Abs(y)
+        If v > Vmax Then Vmax = v
+        TextBox8.Text = Str(Round(t, 2))
+
+    End Sub
+End Class
